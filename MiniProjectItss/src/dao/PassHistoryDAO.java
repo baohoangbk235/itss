f03358ece2691ac4ced8fdf45f199eee400feef3
baohoangbk235 <baohoangbk235@gmail.com>
2019-11-18 00:02:58 +0700

    update

---
+++ MiniProjectItss/src/dao/PassHistoryDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.util.List;
+
+public class PassHistoryDAO<T> {
+	public static void getInfoById() {
+		
+	}
+	
+	public static void updatePassHistoryById() {
+		
+	}
+	
+	public static void insertPassHistory() {
+		
+	}
+}
+ 
+  
