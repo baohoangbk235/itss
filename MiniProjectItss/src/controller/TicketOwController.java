76f2e68aaaee888a967faa41cef9e08e6080a404
Quang Anh Nguyen <anh.nq160152@sis.hust.edu.vn>
2019-11-18 00:07:28 +0700

    Add code to ParrentCOntroller and CardCOntroller and TicketOwController

--- MiniProjectItss/src/controller/TicketOwController.java
+++ MiniProjectItss/src/controller/TicketOwController.java
@@ -1,12 +1,33 @@
 package controller;
 
 import java.util.List;
+
+import dao.StationDAO;
 import dao.TicketOnewayDAO;
+import dto.StationDTO;
 import dto.TicketOnewayDTO;
 
-public class TicketOwController {
+public class TicketOwController extends ParentController {
 
 	public static List<TicketOnewayDTO> getAll(){
 		return TicketOnewayDAO.getAll();
 	}
+
+	public static TicketOnewayDTO getTicket(String ID) { return TicketOnewayDAO.getTkowById(ID);}
+
+	public boolean checkEnterStation(String stationID, TicketOnewayDTO oneWayTicket) {
+		StationDTO enterStation = StationDAO.getStationById(stationID);
+		StationDTO startStation = StationDAO.getStationById(oneWayTicket.getStart_station());
+		StationDTO endStation = StationDAO.getStationById(oneWayTicket.getExit_station());
+		if (enterStation.getDistance() >= startStation.getDistance() && endStation.getDistance() <= endStation.getDistance()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean checkStatus (TicketOnewayDTO oneWayTicket) {
+		return oneWayTicket.getStatus();
+	}
+
 }
--- MiniProjectItss/src/util/Constants.java
+++ MiniProjectItss/src/util/Constants.java
@@ -6,8 +6,11 @@ public final class Constants {
 	public static final String DB_USERNAME = "pnb";
 	public static final String DB_PASSWORD = "";
 	public static final String DB_DATABASE = "AFC_System";
-	
-	public static final Double BASE_FARE = 1.9;
-	public static final Double EXTRA_FARE = 0.4;
+
+
+	public static final int BASE_DISTANCE = 5;
+	public static final double BASE_FARE = 1.9;
+	public static final int EXTRA_DISTANCE = 2;
+	public static final double EXTRA_FARE = 0.4;
 	private Constants() {}
 }
