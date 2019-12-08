bebbee042bdddd4b93301ce8854c9e668ea8f9fd
HikPb <p.ngocba@gmail.com>
2019-12-08 11:10:33 +0700

    update

---
+++ FinalProject/SourceCode/test/dto/CardsDTOTest.java
@@ -0,0 +1,64 @@
+package dto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardsDTOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetCard_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCard_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOwner_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetOwner_id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRelease_time() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRelease_time() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLast_pass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetLast_pass() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ FinalProject/SourceCode/test/dto/ListTicketDTOTest.java
@@ -0,0 +1,34 @@
+package dto;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ListTicketDTOTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetType() {
+		fail("Not yet implemented");
+	}
+
+}
